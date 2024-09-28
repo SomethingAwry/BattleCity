73919977577ec543d71c966a36918217c43286c5
Something Awry <ry.beegle@live.com>
2024-09-27 23:55:57 -0700

    thingy

--- Model/Apple.fs
+++
@@ -1,6 +0,0 @@
-﻿namespace BattleCity.Model;
-
-using Avalonia;
-
-public class Apple(Point location) : GameObject(location) {
-}
--- Model/CellLocation.fs
+++
@@ -1,9 +0,0 @@
-namespace BattleCity.Model;
-
-using Avalonia;
-
-public readonly record struct CellLocation(int X, int Y) {
-    public Point ToPoint() {
-        return new(GameField.CellSize * X, GameField.CellSize * Y);
-    }
-}
